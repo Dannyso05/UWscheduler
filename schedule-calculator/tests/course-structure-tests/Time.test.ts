d979d90687f442dff68ce678446ae3c7911f3388
Alejandro Comas Leon <alecomasleon@gmail.com>
2024-01-13 22:23:13 -0500

    added course-structure-tests

---
+++ schedule-calculator/tests/course-structure-tests/Time.test.ts
@@ -0,0 +1,39 @@
+import Time from '../../src/course-structure/Time'
+
+describe("Time", () => {
+    const time1 = new Time(4, 30);
+    const time2 = new Time(5, 40);  
+
+    it('Test not equal', () => {
+        expect(time1.equals(time2)).toBe(false);
+    });
+
+    it('Test equals and copy', () => {
+        expect(time1.equals(time1.copy())).toBe(true);
+    });
+
+    it('Test equals', () => {
+        expect(time1.equals(new Time(4, 30))).toBe(true);
+    });
+
+    it('Test afterThan 1', () => {
+        expect(time1.afterThan(time2)).toBe(false);
+    });
+
+    it('Test afterThan 2', () => {
+        expect(time2.afterThan(time1)).toBe(true);
+    });
+
+    it('Test afterThan 3', () => {
+        expect(time1.afterThan(time1)).toBe(false);
+    });
+
+    it('Test get hours 1', () => {
+        expect(time1.getHours()).toBe(4);
+    });
+
+    it('Test get hours 2', () => {
+        expect(time2.getHours()).toBe(5);
+    });
+})
+
---
+++ schedule-calculator/tests/course-structure-tests/Timeslot.test.ts
@@ -0,0 +1,31 @@
+import Timeslot from '../../src/course-structure/Timeslot'
+import Time from '../../src/course-structure/Time'
+
+
+describe("Timeslot", () => {
+    let timeslot1: Timeslot = new Timeslot(new Time(4, 30), new Time(5, 20));
+    let timeslot2: Timeslot = new Timeslot(new Time(5, 0), new Time(5, 50));
+    let timeslot3: Timeslot = new Timeslot(new Time(4, 40), new Time(5, 10));
+    let timeslot4: Timeslot = new Timeslot(new Time(3, 40), new Time(4, 10));
+    let timeslot5: Timeslot = new Timeslot(new Time(4, 10), new Time(5, 10));
+
+    it('Test does overlap 1', () => {
+        expect(timeslot1.doesOverlap(timeslot2)).toBe(true);
+    });
+
+    it('Test does overlap 2', () => {
+        expect(timeslot3.doesOverlap(timeslot1)).toBe(true);
+    });
+
+    it('Test does not overlap', () => {
+        expect(timeslot4.doesOverlap(timeslot1)).toBe(false);
+    });
+
+    it('Test copy and equals', () => {
+        expect(timeslot1.equals(timeslot1.copy())).toBe(true);
+    });
+
+    it('Test overlap edge case', () => {
+        expect(timeslot5.doesOverlap(timeslot4)).toBe(false);
+    });
+})
